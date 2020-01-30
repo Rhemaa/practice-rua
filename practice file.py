45a6fb80a65a9e274b41c8643f3531618c19871d
Rhema Anazonwu <ruanazonwu@gmail.com>
2020-01-29 20:19:48 -0600

    this code won't run

--- practice file.py
+++ practice file.py
@@ -5,6 +5,9 @@ Spyder Editor
 This is a temporary script file.
 """
 
-name = "Rhema"
 
-print(name)
+def say_hey(name):
+    saying_hey = "Hey " + name + " , how you living?"
+    return saying_hey
+
+say_hey("Rhema")
